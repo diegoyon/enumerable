2c9c41cdbd211932b905ede5d0982ac0469d19f0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-09-23 00:05:25 +0300

    add the rest of code & fix linter error

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require_relative './my_enumerable.rb'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -6,23 +6,20 @@ class MyList
   def initialize(*list)
     @list = list
   end
-  
-  def each
-    @list.each do |c|
-      yield c
-    end
-  end
 
+  def each(&block)
+    @list.each { |c| block.call(c) }
+  end
 end
 
-list = MyList.new(1,2,3,4)
+list = MyList.new(1, 2, 3, 4)
 
-p list.all? {|e| e < 5}
+p(list.all? { |e| e < 5 })
 
-p list.all? {|e| e > 5}
+p(list.all? { |e| e > 5 })
 
-p list.any? {|e| e == 2}
+p(list.any? { |e| e == 2 })
 
-p list.any? {|e| e == 5}
+p(list.any? { |e| e == 5 })
 
-p list.filter {|e| e.even?}
+p(list.filter(&:even?))
